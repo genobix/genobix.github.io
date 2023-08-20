7ecfa7ee6ac1854b7bae416eb1a73460886acb15
genobix <142700604+genobix@users.noreply.github.com>
2023-08-19 20:46:20 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the "Learn More" button element
+const learnMoreBtn = document.getElementById("learn-more-btn");
+
+// Add a click event listener to the button
+learnMoreBtn.addEventListener("click", () => {
+    alert("Thank you for your interest!");
+});
